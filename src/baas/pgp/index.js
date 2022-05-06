e4fa70ee89c854302442741b2211963d4f890f4f
Brandon Hedge <bhedge@lineagefn.com>
2022-05-06 17:33:52 -0500

    pgp module completed, adds encrypt functionality, and decrypt funtionality for ASCII Armored or Binary PGP encrypted files with automatic detection

--- src/baas/pgp/index.js
+++ src/baas/pgp/index.js
@@ -34,7 +34,6 @@ async function encrypt(VENDOR, message) {
     let encrypted = false;
     let keys = await getKeys(VENDOR)
 
-    // logger.log({ level: 'info', message: 'processing file encryption...' })
     encrypted = await openpgp.encrypt({
         message: await openpgp.createMessage({ text: message }), // input as Message object
         encryptionKeys: keys.vendor.publicKey,
@@ -46,7 +45,6 @@ async function encrypt(VENDOR, message) {
 }
 
 async function decrypt(VENDOR, encrypted) {
-    //logger.log({ level: 'info', message: `processing file decryption to file path [${filePathOutput}]...` })
     let keys = await getKeys(VENDOR)
 
     let message
@@ -75,63 +73,21 @@ async function decrypt(VENDOR, encrypted) {
 }
 
 async function decryptBinary(VENDOR, sourceFilePath) {
-    let asciiArmorMessage = fs.readFileSync(sourceFilePath, {encoding: 'base64'})
-    asciiArmorMessage = `-----BEGIN PGP MESSAGE-----\n\n` + asciiArmorMessage + `\n-----END PGP MESSAGE-----`
-
     let keys = await getKeys(VENDOR)
 
-    let message
-    let decrypted
+    let binaryMessage = fs.readFileSync(sourceFilePath)
+    const encryptedMessage = await openpgp.readMessage({ 
+        binaryMessage: binaryMessage
+    })
 
-    // try {
-        message = await openpgp.readMessage({
-            armoredMessage: asciiArmorMessage // parse armored message
-        });
+    const {data} = await openpgp.decrypt({
+        message: encryptedMessage,
+        decryptionKeys: keys.lineage.privateKey, // for decryption,
+        format: 'binary'
+    });
 
-        decrypted = await openpgp.decrypt({
-            message: message,
-            verificationKeys: keys.lineage.publicKey, // optional
-            decryptionKeys: keys.lineage.privateKey
-        });
-
-        console.log('decrypted:', decrypted)
-        return decrypted.data
-    // } catch (error) {
-    //     if(error.message != 'Misformed armored text') {
-    //         throw (error.message)
-    //     } else {
-    //         throw (error)
-    //     }
-    // }
-
-    // var binaryMessage = await openpgp.readMessage({binaryMessage: binaryEncrypted});
-    //let encryptedUint8 = new TextEncoder().encode(binaryEncrypted);
-    // var encryptedMessage = openpgp.message.read(encryptedUint8)
-
-    // const encryptedMessage = await openpgp.readMessage({ 
-    //     binaryMessage: encryptedUint8
-    // })
-
-    // const encryptedMessage = await openpgp.readMessage({
-    //       binaryMessage: binaryEncrypted // parse encrypted bytes
-    // });
-
-//     const {data} = await openpgp.decrypt({
-//         message: encryptedMessage,
-//         decryptionKeys: keys.lineage.privateKey, // for decryption,
-//         format: 'binary'
-//    });
-
-    // const { data: decrypted } = await openpgp.decrypt({
-    //     message: binaryMessage,
-    //     decryptionKeys: keys.lineage.privateKey,
-    //     config: {
-    //         allowInsecureDecryptionWithSigningKeys: true,
-    //     },
-    //     format: 'binary' // output as Uint8Array
-    // });
-
-   // return data
+    let decrypted = new TextDecoder().decode(data);
+    return decrypted
 }
 
 async function encryptFile(VENDOR, sourceFilePath, destinationFilePath) {
@@ -152,19 +108,29 @@ async function decryptFile(VENDOR, sourceFilePath, destinationFilePath) {
         }
     }
 
-    let sourceFile = fs.readFileSync(sourceFilePath, {encoding:'utf8', flag:'r'})
     try{
-        let decryptedFile = await decrypt(VENDOR, sourceFile)
-        fs.writeFileSync(destinationFilePath, decryptedFile, {encoding:'utf8', flag:'w'})
-        return true
+        let isArmoredFile = await isArmoredCheck(sourceFilePath)
+        if (isArmoredFile) {
+            let sourceFile = fs.readFileSync(sourceFilePath, {encoding:'utf8', flag:'r'})
+            let decryptedFile = await decrypt(VENDOR, sourceFile)
+            fs.writeFileSync(destinationFilePath, decryptedFile, {encoding:'utf8', flag:'w'})
+            return true
+        } else {
+            /* perform a binary decrypt, the file may not be ASCII armored */
+            let decryptedFile = await decryptBinary(VENDOR, sourceFilePath)
+            fs.writeFileSync(destinationFilePath, decryptedFile, {encoding:'utf8', flag:'w'})
+            return true
+        }
     } catch (error) {
-        if (error.message != 'Misformed armored text') throw (error.message)
-
-        /* perform a binary decrypt, the file may not be ASCII armored */
-        let decryptedFile = await decryptBinary(VENDOR, sourceFilePath)
-        fs.writeFileSync(destinationFilePath, decryptedFile, {encoding:'utf8', flag:'w'})
-        return true
+        console.error('ERROR:', error)
+        return false
     }
+}
+
+async function isArmoredCheck(sourceFilePath) {
+    let sourceFile = fs.readFileSync(sourceFilePath, {encoding:'utf8', flag:'r'})
+    if (sourceFile.startsWith('-----BEGIN PGP MESSAGE-----')) return true
+    return false
 }
 
 module.exports.encrypt = (VENDOR, message) => {
@@ -187,34 +153,6 @@ module.exports.decryptFile = (VENDOR, sourceFilePath, destinationFilePath = null
     return decryptFile(VENDOR, sourceFilePath, destinationFilePath)
 }
 
-
-// async function decryptFiles(logger, folderMappings, publicKey, privateKey){
-//     for (const mapping of folderMappings) {
-//         if (mapping.type == 'get') {
-//             // get an array of the local files to evaluate
-//             let filenames = await getLocalFileList(mapping.destination)
-
-//             for (const filename of filenames) {
-//                 let hasSuffixGPG = ( filename.split('.').pop().toLowerCase() == 'gpg' ) 
-
-//                 if (hasSuffixGPG) {
-//                     logger.log({ level: 'info', message: `${VENDOR_NAME}: GPG DECRYPT [${filename}] located at ${mapping.destination}] on [LFNSRVFKNBANK01 attempting decrypt...` })
-//                     // ** Procede to Decrypt the File **
-
-//                     const filePathInput = mapping.destination + '/' + filename;
-//                     let filePathOutput = mapping.destination + '/' + filename;
-//                     filePathOutput = filePathOutput.substring(0, filePathOutput.indexOf('.gpg'))
-
-//                     // pull the encrypted message into a file
-//                     let encrypted = fs.readFileSync(filePathInput, {encoding:'utf8', flag:'r'})
-
-//                     //1. Decrypt
-//                     let wasDecrypted = await decryptFile(logger, encrypted, filePathOutput, publicKey, privateKey)
-
-//                     //2. Delete the original .gpg file ( there is still a backup in the audit folder if it needs to process again )
-//                     if (wasDecrypted) { deleteLocalFile(logger, filePathInput) }
-//                 }
-//             }
-//         }
-//     }
-// }
+module.exports.isArmoredCheck = (sourceFilePath) => {
+    return isArmoredCheck(sourceFilePath)
+}
