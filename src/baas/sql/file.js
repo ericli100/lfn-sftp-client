ea73af17cdf73a8c2b7f765f1da356c939235748
Brandon Hedge <bhedge@lineagefn.com>
2022-05-31 14:00:29 -0500

    fix: adds the default Tenant ID to an ENV variable to remove from hard coding from the spike code

--- src/baas/sql/file.js
+++ src/baas/sql/file.js
@@ -5,7 +5,7 @@
 function Handler(mssql) {
     Handler.exists = async function fileExists(sha256) {
         if (!sha256) throw ('sha256 required')
-        let tenantId = `3e2e6220-edf2-439a-91e4-cef6de2e8b7b`
+        let tenantId = process.env.PRIMAY_TENANT_ID
     
         let sqlStatement = `SELECT [entityId]
         FROM [baas].[files]
@@ -31,7 +31,7 @@ function Handler(mssql) {
         if (!contextOrganizationId) throw ('contextOrganizationId required')
         if (!fileName) throw ('fileName required')
     
-        let tenantId = `3e2e6220-edf2-439a-91e4-cef6de2e8b7b`
+        let tenantId = process.env.PRIMAY_TENANT_ID
         let sqlStatement = `
         INSERT INTO [baas].[files]
                ([entityId]
