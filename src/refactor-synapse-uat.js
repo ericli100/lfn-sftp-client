f32586b9095b96ac46db080600fe2fdc90b7e4e2
Brandon Hedge <bhedge@lineagefn.com>
2022-07-02 13:12:42 -0500

    adds synapse UAT refactor

--- src/refactor-synapse-uat.js
+++ src/refactor-synapse-uat.js
@@ -28,6 +28,8 @@ const logger = createLogger({
 });
 
 async function main(){
+    let ENABLE_FTP_PULL = false // dev time variable
+
     let args = {};
     let BAAS = require('./baas')(args)
     let baas = await BAAS
@@ -38,25 +40,28 @@ async function main(){
     await baas.sftp.setConfig( config )
     await baas.sftp.setLogger(logger)
 
-    await baas.audit.log({baas, logger, level: 'info', message: `SFTP Processing started for [${VENDOR_NAME}] for environment [${ENVIRONMENT}] on [${config.server.host}] for PROCESSING_DATE [${PROCESSING_DATE}]...`})
+    let correlationId = await baas.id.generate()
+
+    if(ENABLE_FTP_PULL){
+        await baas.audit.log({baas, logger, level: 'info', message: `SFTP Processing started for [${VENDOR_NAME}] for environment [${ENVIRONMENT}] on [${config.server.host}] for PROCESSING_DATE [${PROCESSING_DATE}]...`})
+        // ** GET FILES FROM EMAIL
+        // -- SET CONFIG TO PARSE FROM EMAIL ADDRESS
     
+        // ** LIST FILE ON REMOTE SFTP
+        let remoteFiles = await baas.processing.listRemoteSftpFiles(baas, logger, VENDOR_NAME, ENVIRONMENT, config)
+        await baas.audit.log({baas, logger, level: 'info', message: `SFTP there are (${remoteFiles.length}) remote files for [${VENDOR_NAME}] for environment [${ENVIRONMENT}] on [${config.server.host}] with details of [${JSON.stringify(remoteFiles).replace(/[\/\(\)\']/g, "' + char(39) + '" )}].`})
+    
+        let remoteValidatedFiles = await baas.processing.getRemoteSftpFiles(baas, logger, VENDOR_NAME, ENVIRONMENT, config, remoteFiles)
+        await baas.audit.log({baas, logger, level: 'info', message: `SFTP [GET] VALIDATED (${remoteValidatedFiles.validatedRemoteFiles.length}) remote files for [${VENDOR_NAME}] for environment [${ENVIRONMENT}] on [${config.server.host}] with details of [${JSON.stringify(remoteValidatedFiles.validatedRemoteFiles).replace(/[\/\(\)\']/g, "' + char(39) + '" )}] and loaded them into the database.`})
+        
+         ////////   await baas.processing.removeRemoteSftpFiles(baas, logger, VENDOR_NAME, ENVIRONMENT, config, remoteValidatedFiles.validatedRemoteFiles)
+        
+        await baas.audit.log({baas, logger, level: 'info', message: `SFTP Processing ended for [${VENDOR_NAME}] for environment [${ENVIRONMENT}] on [${config.server.host}] for PROCESSING_DATE [${PROCESSING_DATE}].`})
+    }
 
-    // ** GET FILES FROM EMAIL
-    // -- SET CONFIG TO PARSE FROM EMAIL ADDRESS
-
-    // ** LIST FILE ON REMOTE SFTP
-    let remoteFiles = await baas.processing.listRemoteSftpFiles(baas, logger, VENDOR_NAME, ENVIRONMENT, config)
-    await baas.audit.log({baas, logger, level: 'info', message: `SFTP there are (${remoteFiles.length}) remote files for [${VENDOR_NAME}] for environment [${ENVIRONMENT}] on [${config.server.host}] with details of [${JSON.stringify(remoteFiles)}].`})
-
-    let remoteValidatedFiles = await baas.processing.getRemoteSftpFiles(baas, logger, VENDOR_NAME, ENVIRONMENT, config, remoteFiles)
-    await baas.audit.log({baas, logger, level: 'info', message: `SFTP [GET] VALIDATED (${remoteValidatedFiles.validatedRemoteFiles.length}) remote files for [${VENDOR_NAME}] for environment [${ENVIRONMENT}] on [${config.server.host}] with details of [${JSON.stringify(remoteValidatedFiles.validatedRemoteFiles)}] and loaded them into the database.`})
-
- ////////   await baas.processing.removeRemoteSftpFiles(baas, logger, VENDOR_NAME, ENVIRONMENT, config, remoteValidatedFiles.validatedRemoteFiles)
-    await baas.processing.processInboundFilesFromDB(baas, logger, VENDOR_NAME, ENVIRONMENT)
+    await baas.processing.processInboundFilesFromDB(baas, logger, VENDOR_NAME, ENVIRONMENT, config, correlationId)
     await baas.processing.processOutboundFilesFromDB(baas, logger, VENDOR_NAME, ENVIRONMENT)
 
-    // set the workflow items
-    // -- not processed (used for Import)
     // -- receiptSent (used for FileActivityFile)
 
     // ** TODO: await baas.processing.putRemoteSftpFiles
@@ -64,7 +69,7 @@ async function main(){
     // TODO: generate email NOTIFICATIONS
     // TODO: send email notifications
 
-    await baas.audit.log({baas, logger, level: 'info', message: `SFTP Processing ended for [${VENDOR_NAME}] for environment [${ENVIRONMENT}] on [${config.server.host}] for PROCESSING_DATE [${PROCESSING_DATE}].`})
+    
 
     console.log('sql: disconnecting...')
     baas.sql.disconnect()
