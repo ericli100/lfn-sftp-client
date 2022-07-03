d72364cbd985342c6cffefb1a5b485e896ecf01d
Brandon Hedge <bhedge@lineagefn.com>
2022-07-02 20:53:17 -0500

    removes comments - Wire

--- src/baas/wire/index.js
+++ src/baas/wire/index.js
@@ -8,27 +8,6 @@
  copied the necessary bits and ignored their stupid intent.
 */
 
-/*
-globalThis.require = require;
-globalThis.fs = require("fs");
-globalThis.TextEncoder = require("util").TextEncoder;
-globalThis.TextDecoder = require("util").TextDecoder;
-
-globalThis.performance = {
-	now() {
-		const [sec, nsec] = process.hrtime();
-		return sec * 1000 + nsec / 1000000;
-	},
-};
-
-const crypto = require("crypto");
-globalThis.crypto = {
-	getRandomValues(b) {
-		crypto.randomFillSync(b);
-	},
-};
-*/
-
 const fs = require('fs');
 const path = require('path');
 
@@ -61,47 +40,7 @@ async function parse () {
         throw('Invalid Wire File!')
     }
 
-    // WebAssembly.instantiate(wasmBuffer).then(wasmModule => {
-    //     // Exported function live under instance.exports
-    //     const { parseContents } = wasmModule.instance.exports;
-    //     let input = inputFile()
-    //     const output = parseContents(input)
-    //     console.log(output);
-    // });
-    
-    // const wasmModule = await loader.instantiateStreaming(new Uint8Array(wasm).buffer, go.importObject);
-
-        // WebAssembly.instantiate(wasmBuffer).then(wasmModule => {
-        //   // Exported function live under instance.exports
-        //   const { add } = wasmModule.instance.exports;
-        //   const sum = add(5, 6);
-        //   console.log(sum); // Outputs: 11
-        // });
-
-    // let moovWire = await go.run(wire);
-    // const moovWire = await WebAssembly.instantiate(mod, go.importObject);
-    // await go.run(moovWire.instance);
-    
- ///   let input = inputFile()
-
-  ///  let output = parseContents(input)
-    // const mod = await wasm_exec.WebAssembly.compile(readFileSync('./wire.wasm'));
-    // let inst = await wasm_exec.WebAssembly.instantiate(mod, go.importObject);
-
-  ///  console.clear();
-    //await go.run(inst);
-   // inst = await wasm_exec.WebAssembly.instantiate(mod, go.importObject);
-
-
-
-    //let output = parseContents(input)
-
-    // console.log (output)
-    // return output;
-
 }
-
-// await parse();
 
 function inputFile() {
     let sampleWire = path.resolve('./src/baas/wire/sample_wire.txt')
@@ -122,64 +61,5 @@ function checkFileType(){
 }
 
 module.exports.parse = parse
-
-
-// JS
-// copy
-// // Assume add.wasm file exists that contains a single function adding 2 provided arguments
-// const fs = require('fs');
-
-// const wasmBuffer = fs.readFileSync('/path/to/add.wasm');
-// WebAssembly.instantiate(wasmBuffer).then(wasmModule => {
-//   // Exported function live under instance.exports
-//   const { add } = wasmModule.instance.exports;
-//   const sum = add(5, 6);
-//   console.log(sum); // Outputs: 11
-// });
-
-
-
-// const json = function(input) {
-//     jsonoutput.value = parseContents(input)
-//     jsonoutput.setSelectionRange(0,0)
-//     jsonoutput.focus()
-// }
-
-// const clearForms = function() {
-//     jsoninput.value = ""
-//     jsonoutput.value = ""
-//     document.getElementById('input-file').value = ''
-// }
-// wireform.addEventListener('submit', (event) => {
-//     event.preventDefault();
-//     json(jsoninput.value)
-// })
-
-// document.getElementById('input-file')
-//     .addEventListener('change', parseFromFile)
-
-// function parseFromFile(event) {
-//     const input = event.target
-//     if ('files' in input && input.files.length > 0) {
-//         placeFileContent(
-//             document.getElementById('jsonoutput'),
-//             input.files[0])
-//     }
-// }
-
-// function placeFileContent(target, file) {
-//     readFileContent(file).then(content => {
-//         json(content)
-//     }).catch(error => console.log(error))
-// }
-
-// function readFileContent(file) {
-//     const reader = new FileReader()
-//     return new Promise((resolve, reject) => {
-//         reader.onload = event => resolve(event.target.result)
-//         reader.onerror = error => reject(error)
-//         reader.readAsText(file)
-//     })
-// }
 
 
