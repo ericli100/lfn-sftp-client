6979944679521a434547bf7600633e6bc5397e52
Brandon Hedge <bhedge@lineagefn.com>
2022-07-04 13:33:09 -0500

    adds the running total and proper parsing of multiple wires in the same file

--- src/baas/wire/index.js
+++ src/baas/wire/index.js
@@ -10,19 +10,28 @@
 
 const fs = require('fs');
 const path = require('path');
+const { EOL } = require('os');
+const readline = require('readline');
+const events = require('events');
 
 // fetch this file from: => https://oss.moov.io/wire/wire.wasm
 let wasmPath = path.resolve('./src/baas/wire/wire.wasm')
 const wasmBuffer = fs.readFileSync( wasmPath );
 
-async function parse () {
+async function parse ( inputfile ) {
     require('./wasm_exec.js');
     const go = new Go();
     const importObject = go.importObject;
     const wasm = await WebAssembly.instantiate(wasmBuffer, importObject);
     go.run(wasm.instance)
 
-    let input = inputFile()
+    // if(!inputfile) inputfile = './src/baas/wire/sample_wire.txt'
+    if(!inputfile) inputfile = './src/baas/wire/wire_fed_20220623132146_0.txt'
+
+    let input = await inputFileToString( inputfile )
+    let parsedWireInput = await parseWireFile( inputfile )
+
+    console.log('parsedWireInput:', parsedWireInput)
 
     let output = ''
     
@@ -44,23 +53,140 @@ async function parse () {
 
 }
 
-function inputFile() {
-    let sampleWire = path.resolve('./src/baas/wire/sample_wire.txt')
+async function inputFileToString ( inputfile ) {
+    let sampleWire = path.resolve( inputfile )
     let output = fs.readFileSync( sampleWire )
     return output.toString()
 }
 
 
-function checkFileType(){
+async function parseWireFile( inputfile ) {
+    let output = {}
     // TODO: check if YFT811 or {1500}
     // -- parse off the prefix
+    output.hasYFT811 = false
+    output.YFT811count = 0
 
     // TODO: check if the wires are on a single line or multiples
     // -- parse to multiline
+    output.isMultiline = false
+    output.linesCount = 0
 
     // TODO: check if Multiple Wires in the same file.
     // -- Return an Array of parsed wires
+    output.hasMultipleWires = false
+    output.wiresCount = 0
+
+    // contains an array of wires
+    output.wiresJSON = []
+    
+    // dollar total for all wires
+    output.totalCredits = 0
+    output.totalDebits = 0
+    output.currency = 'USD'
+
+    // strip out the YFT811
+    // new lines after each line identifier {???}
+    output.reformatedFile = ''
+
+    output.parsedFileArray = []
+    
+    let currentLine = 0
+    let currentWireJSON = {}
+
+    try {
+      const rl = readline.createInterface({
+        input: fs.createReadStream( inputfile ),
+        crlfDelay: Infinity
+      });
+  
+      rl.on('line', (line) => {
+        currentLine++
+        output.totalLines = currentLine;
+
+        // it will be easier to deal with an array versus reacting to events
+        output.parsedFileArray.push(line);
+      });
+  
+      await events.once(rl, 'close');
+
+      // lets work with the array that was just processed:
+      for(const i in output.parsedFileArray){
+        let line = output.parsedFileArray[i]
+        // process each line of the file
+          
+        console.log('line includes YFT811:', line.includes('YFT811') )
+        if(line.includes('YFT811')) {
+            output.hasYFT811 = true
+            output.YFT811count = output.YFT811count + 1
+        }
+
+        console.log('line includes {1500}:', line.includes('{1500}') )
+        if(line.includes('{1500}')) {
+            output.wiresCount = output.wiresCount + 1;
+            if(output.wiresCount > 1) output.hasMultipleWires = true
+        }
+
+        // if(line.includes('{1500}') && output.wiresCount) {
+        //     // this is the start of a new wire
+        //     output.wiresJSON.push(currentWireJSON)
+        //     // reset the current JSON
+        //     currentWireJSON = {}
+        // }
+
+        // parse the file based
+        let parsedWire = line.split('{')
+
+        for( const pi in parsedWire) {
+            let parsedWire2 = parsedWire[pi].split('}')
+
+            if( (parsedWire2[0].trim() == '1500' || parsedWire2[0].trim() == 'YFT811' ) && output.hasMultipleWires && pi < 1) {
+                // we detected an additional wire, write what we have
+                currentWireJSON.totalCredits = parseInt(currentWireJSON["'{2000}'"]) || 0
+                currentWireJSON.totalDebits = 0
+                currentWireJSON.currency = 'USD'
+
+                // running total
+                output.totalCredits = output.totalCredits + currentWireJSON.totalCredits;
+
+                output.wiresJSON.push(currentWireJSON)
+                currentWireJSON = {}
+            }
+
+            currentWireJSON[`'{${parsedWire2[0].trim()}}'`] = parsedWire2[1] || ''
+
+            if(i == output.parsedFileArray.length - 1 && pi == parsedWire.length - 1) {
+                // this is the last wire at the end of the array
+                currentWireJSON.totalCredits = parseInt(currentWireJSON["'{2000}'"]) || 0
+                currentWireJSON.totalDebits = 0
+                currentWireJSON.currency = 'USD'
+
+                // running total
+                output.totalCredits = output.totalCredits + currentWireJSON.totalCredits;
+
+                output.wiresJSON.push(currentWireJSON)
+                currentWireJSON = {}
+            }
+        }
+      }
+
+      // output.reformatedFile += `${fixedLine}${EOL}`
+
+    //   console.log('parsedWire:', parsedWire)
+    //   output.parsedFileArray.concat(parsedWire)
+
+
+    console.log(output)
+
+    } catch (err) {
+      console.error(err);
+      throw(err)
+    }
+
+    return output
 }
+
+
 
 module.exports.parse = parse
 
