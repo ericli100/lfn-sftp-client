0a5befa80d2934236ede008458744631b0b146a5
Richard Brown <richard@numano.de>
2022-07-23 01:40:04 -0500

    Refactor entityid and adds first command.

--- src/entityId/index.js
+++ src/entityId/index.js
@@ -2,33 +2,17 @@
 
 const intformat = require('biguint-format');
 const FlakeId = require('flake-idgen');
-const flakeId = new FlakeId();
 
-async function generateFlakeId () {
-  let newFlakeId = await intformat(flakeId.next(), 'hex');
-  return newFlakeId;
-}
+const fidc = process.env.FLAKEID_DATACENTER;
+const fiw = process.env.FLAKEID_WORKER;
+
+const datacenter = fidc && !isNaN(Number.parseInt(fidc)) ? Number.parseInt(fidc) : 0;
+const worker = fiw && !isNaN(Number.parseInt(fiw)) ? Number.parseInt(fiw) : 0;
+
+const flakeId = new FlakeId({ datacenter, worker });
 
 function generate() {
-    return intformat(flakeId.next(), 'hex')
+    return intformat(flakeId.next(), 'hex');
 }
 
-async function main() {
-    let newId = await generateFlakeId()
-    console.log(newId)
-}
-
-// adds a main function call to export a FlakeId on demand from the command line.
-main()
-
-module.exports = function constructor () {
-    function getFlakeId() {
-        return Promise.resolve(generateFlakeId());
-    }
-
-    return {
-        getFlakeId
-    };
-};
-
-module.exports.generate = generate
+module.exports = { generate };
