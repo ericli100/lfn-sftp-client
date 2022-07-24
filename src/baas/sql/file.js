00332b774acf06c132ba3d1ebbfca996b47ea5ed
Brandon Hedge <bhedge@lineagefn.com>
2022-07-24 10:44:09 -0500

    adds: baas.sql.file.setIsVaultValidated function

--- src/baas/sql/file.js
+++ src/baas/sql/file.js
@@ -387,6 +387,41 @@ function Handler(mssql) {
             return output
     }
 
+    Handler.setIsVaultValidated = async function setIsVaultValidated( {entityId, contextOrganizationId, correlationId} ){
+        let output = {}
+
+        let mutatedBy = 'SYSTEM'
+
+        if (!entityId) throw ('entityId required')
+        let tenantId = process.env.PRIMAY_TENANT_ID
+        if (!contextOrganizationId) throw ('contextOrganizationId required')
+
+        let sqlStatement = `
+            UPDATE [baas].[files]
+            SET [isVaultValidated] = 1
+                ,[hasProcessingErrors] = 0
+                ,[correlationId] = '${correlationId}'
+                ,[mutatedBy] = '${mutatedBy}'
+                ,[mutatedDate] = (SELECT getutcdate())
+            WHERE [entityId] = '${entityId}' 
+            AND [tenantId] = '${tenantId}'
+            AND [contextOrganizationId] = '${contextOrganizationId}';`
+
+            let param = {}
+            param.params = []
+            param.tsql = sqlStatement
+            
+            try {
+                let results = await mssql.sqlQuery(param);
+                output = results.data
+            } catch (err) {
+                console.error(err)
+                throw err
+            }
+    
+            return output
+    }
+
     Handler.setFileSentToDepositOps = async function setFileSentToDepositOps( {entityId, contextOrganizationId, correlationId} ){
         let output = {}
 
