42ce94147a478a7665a99064dc1c904225195dd9
Brandon Hedge <bhedge@lineagefn.com>
2022-07-28 17:59:00 -0500

    adds: new mime type collection on *nix environments for unknown types. Types are known as they are stored in a mimeType.json file with a faast implementation of a magic bytes lookup. This will allow for a way to process the known types on Win32 and will throw an error for unknown types. A further list and more accurate magic bytes can be used in the future. This is an initial push of code to get this running on the server.

--- src/baas/pgp/index.js
+++ src/baas/pgp/index.js
@@ -9,6 +9,15 @@ const path = require('path');
 const  { detectFileMime, detectBufferMime } = require('mime-detect');
 const eol = require('eol')
 
+const os = require('node:os');
+const {readFile, writeFile} = require('fs/promises');
+
+var mimeCache = {
+    magicNumbers: [
+        {magicNumber: "unknown", mimeType: "unknown"}
+    ]
+};
+
 async function getKeys(VENDOR, ENVIRONMENT) {
     try{
         let LINEAGE = 'lineage'
@@ -101,6 +110,78 @@ async function encryptFile(VENDOR, ENVIRONMENT, sourceFilePath, destinationFileP
     return true
 }
 
+const getMagicBytes = async( filePath )=>{
+    const result = await readFile(filePath,'binary')
+    const magicbytes = Buffer.from(result.substring(0, 8))
+    return magicbytes.join('')
+}
+
+const putMimeType = async({ magicbytes, mimeType })=>{
+    // is it already in the cache? Check first
+    let exists = await getMimeType({ magicbytes })
+
+    // write though cache
+    if (!exists) {
+        mimeCache.magicNumbers.push({ magicNumber: magicbytes, mimeType: mimeType })
+        await writeMimeTypeJSON({ mimeTypeJSON: mimeCache })
+    }
+    return mimeCache 
+}
+
+const readMimeTypeJSON = async() => {
+    // read the JSON
+    // return the JSON
+    let mimeTypeJSON = await readFile( path.resolve(__dirname, 'mimeType.json'), 'utf-8' )
+    return JSON.parse( mimeTypeJSON )
+}
+
+const writeMimeTypeJSON = async({ mimeTypeJSON }) => {
+   await writeFile( path.resolve(__dirname, 'mimeType.json'), JSON.stringify(mimeTypeJSON, " "), 'utf-8')
+   return await readMimeTypeJSON()
+}
+
+const getMimeTypeThisOS = async( file, existingMimeType = undefined )=> {
+    let magicbytes = await getMagicBytes( file )
+    let mimeType = existingMimeType
+
+    if(os.platform == 'darwin' || os.platform == 'linux' || os.platform == 'freebsd' || os.platform == 'openbsd') {
+        mimeType = await getMimeType({ magicbytes })
+
+        if(!mimeType) {
+            if (mimeType == 'application/octet-stream') {
+                // we have a buffer / stream, detect the mime type from there
+                mimeType = await detectBufferMime( file )
+            } else {
+                mimeType = await detectFileMime( file )
+            }
+
+            await putMimeType ({ magicbytes, mimeType })
+        }
+    } else {
+        // let's do the lookup from our local cache
+        mimeType = await getMimeType({ magicbytes })
+    }
+
+    return mimeType
+}
+
+const getMimeType = async({ magicbytes })=>{
+    if(mimeCache.magicNumbers.length <= 1) {
+        // populate cache
+        console.log('baas.pgp.getMimeType(): we need to populate the cache...')
+        
+        // read the JSON file
+        // update mimeCache
+        mimeCache = await readMimeTypeJSON()
+    }
+
+    // read from mimeCache
+    console.log('we need to populate the cache')
+    let found = mimeCache.magicNumbers.find(x => x.magicNumber === magicbytes)
+    if(found) return found.mimeType 
+    return undefined 
+}
+
 async function decryptFile({VENDOR, ENVIRONMENT, sourceFilePath, destinationFilePath, baas, audit}) {
     let ALLOW_AUDIT_ENTRIES = false
     let logger
@@ -131,13 +212,15 @@ async function decryptFile({VENDOR, ENVIRONMENT, sourceFilePath, destinationFile
     let mimeType
     let isOctetStream = false
     try{
+        
         // take an initial look to see if the mimeType is text or Binary
-        mimeType = await detectFileMime( sourceFilePath )
+        mimeType = await getMimeTypeThisOS( sourceFilePath )
+        
         if(ALLOW_AUDIT_ENTRIES) await baas.audit.log({baas, logger, level: 'verbose', message: `${audit.vendor}: file [${audit.filename}] mime type was initially detected as [${mimeType}] for environment [${audit.environment}].`, effectedEntityId: audit.entityId, correlationId })
 
         if (mimeType == 'application/octet-stream') {
             // we have a buffer / stream, detect the mime type from there
-            mimeType = await detectBufferMime( sourceFilePath )
+            mimeType = await getMimeTypeThisOS( sourceFilePath, mimeType )
             isOctetStream = true
             if(ALLOW_AUDIT_ENTRIES) await baas.audit.log({baas, logger, level: 'verbose', message: `${audit.vendor}: file [${audit.filename}] mime type is [application/octet-stream] and detected the binary file mime type to be [${mimeType}] for environment [${audit.environment}].`, effectedEntityId: audit.entityId, correlationId  })
         }
@@ -154,6 +237,10 @@ async function decryptFile({VENDOR, ENVIRONMENT, sourceFilePath, destinationFile
             let sourceFile = fs.readFileSync(sourceFilePath, {encoding:'utf8', flag:'r'})
             let decryptedFile = await decrypt(VENDOR, ENVIRONMENT, sourceFile)
             fs.writeFileSync(destinationFilePath, decryptedFile, {encoding:'utf8', flag:'w'})
+
+            // capture the mime type of the decrypted file
+            await getMimeTypeThisOS( destinationFilePath )
+
             return true
         } else {
             if( mimeType == 'application/csv; charset=us-ascii'  || 
@@ -161,7 +248,12 @@ async function decryptFile({VENDOR, ENVIRONMENT, sourceFilePath, destinationFile
                 mimeType.indexOf('charset=us-ascii') > 0         ||
                 mimeType.indexOf('charset=iso-8859-1') > 0          ){
                 let sourceFile = fs.readFileSync(sourceFilePath, {encoding:'utf8', flag:'r'})
+                
                 fs.writeFileSync(destinationFilePath, eol.split(sourceFile).join(eol.lf), {encoding:'utf8', flag:'w'})
+
+                // capture the mime type of the decrypted file
+                await getMimeTypeThisOS( destinationFilePath )
+
                 if(ALLOW_AUDIT_ENTRIES) await baas.audit.log({baas, logger, level: 'warn', message: `${audit.vendor}: baas.pgp.decryptFile() [${audit.filename}] the mime type is [${mimeType}] but we were expecting an encrypted file, the contents were written out to disk. [${audit.environment}].`, effectedEntityId: audit.entityId, correlationId  })
                 
                 return true
@@ -178,11 +270,19 @@ async function decryptFile({VENDOR, ENVIRONMENT, sourceFilePath, destinationFile
                     let decryptedFile = await decryptBinary(VENDOR, ENVIRONMENT, sourceFilePath)
                      // it decrypted... go ahead and write it out.
                     fs.writeFileSync(destinationFilePath, decryptedFile, {encoding:'utf8', flag:'w'})
+
+                    // capture the mime type of the decrypted file
+                    await getMimeTypeThisOS( destinationFilePath )
+
                     return true
                 } catch (quickDecryptError) {
                     // do not inlude the encoding in the read or write
                     let sourceFile = fs.readFileSync(sourceFilePath, {flag:'r'})
                     fs.writeFileSync(destinationFilePath, sourceFile.toString(), {flag:'w'})
+
+                    // capture the mime type of the decrypted file
+                    await getMimeTypeThisOS( destinationFilePath )
+
                     if(ALLOW_AUDIT_ENTRIES) await baas.audit.log({baas, logger, level: 'warn', message: `${audit.vendor}: file [${audit.filename}] the mime type is [${mimeType}] but we were expecting an encrypted file, the contents were written out to the buffer. error:[${quickDecryptError.message}] [${audit.environment}].`, effectedEntityId: audit.entityId, correlationId  })
                     return true
                 }
@@ -191,6 +291,10 @@ async function decryptFile({VENDOR, ENVIRONMENT, sourceFilePath, destinationFile
             /* perform a binary decrypt, the file may not be ASCII armored */
             let decryptedFile = await decryptBinary(VENDOR, ENVIRONMENT, sourceFilePath)
             fs.writeFileSync(destinationFilePath, decryptedFile, {encoding:'utf8', flag:'w'})
+
+            // capture the mime type of the decrypted file
+            await getMimeTypeThisOS( destinationFilePath )
+
             if(ALLOW_AUDIT_ENTRIES) await baas.audit.log({baas, logger, level: 'verbose', message: `${audit.vendor}: file [${audit.filename}] was decrypted to the buffer successfully [${audit.environment}].`, effectedEntityId: audit.entityId, correlationId })
             return true
         }
