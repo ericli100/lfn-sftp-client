6434ca68ac10a769e4e2a462490717689e17dd4a
Brandon Hedge <bhedge@lineagefn.com>
2022-11-01 17:45:14 -0500

    updates: adds new processing flags to Synctera

--- src/refactor-synctera-prd.js
+++ src/refactor-synctera-prd.js
@@ -225,12 +225,11 @@ async function sftpConfig(VENDOR_NAME, ENVIRONMENT) {
     config.processing.ENABLE_FTP_PULL = true
     config.processing.ENABLE_INBOUND_EMAIL_PROCESSING = true
     config.processing.ENABLE_INBOUND_PROCESSING_FROM_DB = true
-    config.processing.ENABLE_OUTBOUND_PROCESSING_FROM_DB = false
-    config.processing.ENABLE_OUTBOUND_EMAIL_PROCESSING = false
+    config.processing.ENABLE_OUTBOUND_PROCESSING_FROM_DB = true
+    config.processing.ENABLE_OUTBOUND_EMAIL_PROCESSING = true
     config.processing.ENABLE_FILE_RECEIPT_PROCESSING = true
-    config.processing.ENABLE_REMOTE_DELETE = false
+    config.processing.ENABLE_REMOTE_DELETE = true
     config.processing.ENABLE_MANUAL_DB_DOWNLOAD = false
-    config.processing.ENABLE_NOTIFICATIONS = true
    
     return config
 }
