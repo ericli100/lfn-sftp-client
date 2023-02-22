cfeaa5f98450b5db18e7cc5accedaadbd309fad7
Brandon Hedge <bhedge@lineagefn.com>
2023-02-22 13:22:54 -0600

    increase the DB timeout

--- src/db/index.js
+++ src/db/index.js
@@ -10,7 +10,7 @@ CONFIG.db = {
     database: process.env.BAAS_DB_DATABASE,
     port: 1433,
     connectionTimeout: 60000,
-    requestTimeout: 240000,
+    requestTimeout: 480000,
     stream: false,
     parseJSON: false,
     pool: {
